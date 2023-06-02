558165f23cfbd03885bde35e19618c238593feaf
@sk <>
2023-06-02 15:30:29 +0530

    commit github demo

---
+++ src/main/java/com/mycompany/mavenproject1new/Mavenproject1new.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1new;
+
+/**
+ *
+ * @author user
+ */
+public class Mavenproject1new {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
